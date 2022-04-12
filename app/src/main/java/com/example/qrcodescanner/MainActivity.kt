6b4afbc2bca05ad25a16ef631ca6b5e34fc54b2f
David Kirovski <41066264+Kirca54@users.noreply.github.com>
2022-04-12 19:06:44 +0200

    added camera and flash

--- app/src/main/java/com/example/qrcodescanner/MainActivity.kt
+++ app/src/main/java/com/example/qrcodescanner/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.qrcodescanner
 
 import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 import androidx.viewpager.widget.ViewPager
 import kotlinx.android.synthetic.main.activity_main.*
@@ -9,6 +11,11 @@ import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        getSupportActionBar()?.hide()
+
         setContentView(R.layout.activity_main)
         setViewPager()
         setBottomViewListener()
--- app/src/main/java/com/example/qrcodescanner/scanner/QrScannerFragment.kt
+++ app/src/main/java/com/example/qrcodescanner/scanner/QrScannerFragment.kt
@@ -5,12 +5,19 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.content.ContextCompat
 import com.example.qrcodescanner.R
+import kotlinx.android.synthetic.main.fragment_qr_scanner.view.*
+import kotlinx.android.synthetic.main.fragment_qr_scanner.*
+import me.dm7.barcodescanner.zbar.Result
+import me.dm7.barcodescanner.zbar.ZBarScannerView
+
 
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-class QrScannerFragment : Fragment() {
+class QrScannerFragment : Fragment(),ZBarScannerView.ResultHandler {
 
    /* private var param1: String? = null
     private var param2: String? = null
@@ -37,9 +44,77 @@ class QrScannerFragment : Fragment() {
              }*/
     }
 
+    private lateinit var mView: View
+
+    lateinit var scannerView: ZBarScannerView
+
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState:Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_qr_scanner, container, false)
+        mView =  inflater.inflate(R.layout.fragment_qr_scanner, container, false)
+        initViews()
+        onClicks()
+        return mView.rootView
+    }
+
+    private fun initViews() {
+        initializeQRCamera()
+    }
+
+    private fun initializeQRCamera() {
+        scannerView = ZBarScannerView(context)
+        scannerView.setResultHandler(this)
+        scannerView.setBackgroundColor(ContextCompat.getColor(context!!, R.color.colorTranslucent))
+        scannerView.setBorderColor(ContextCompat.getColor(context!!, R.color.colorPrimaryDark))
+        scannerView.setLaserColor(ContextCompat.getColor(context!!, R.color.colorPrimaryDark))
+        scannerView.setBorderStrokeWidth(10)
+        scannerView.setSquareViewFinder(true)
+        scannerView.setupScanner()
+        scannerView.setAutoFocus(true)
+        startQRCamera()
+        mView.containerScanner.addView(scannerView)
+    }
+
+
+
+    override fun handleResult(rawResult: Result?) {
+        onQrResult(rawResult?.contents)
+        resetPreview()
+    }
+
+    private fun onQrResult(contents: String?) {
+        Toast.makeText(context!!,contents,Toast.LENGTH_SHORT).show()
+    }
+
+    private fun startQRCamera() {
+        scannerView.startCamera()
+    }
+
+    private fun resetPreview() {
+        scannerView.stopCamera()
+        scannerView.startCamera()
+        scannerView.stopCameraPreview()
+        scannerView.resumeCameraPreview(this)
+    }
+
+    private fun onClicks() {
+        mView.flashToggle.setOnClickListener {
+            if(mView.flashToggle.isSelected){
+                offFlashLight()
+            } else {
+                onFlashLight()
+            }
+        }
+    }
+
+    private fun onFlashLight() {
+        mView.flashToggle.isSelected = true
+        scannerView.flash = true
+    }
+
+    private fun offFlashLight() {
+        mView.flashToggle.isSelected = false
+        scannerView.flash = false
     }
 
 
